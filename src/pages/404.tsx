7d96793578d1f000c7cbfd8f4b8e3a2b6634cc6a
Vraja-nayaka <messiah.turner@gmail.com>
2020-08-17 11:38:21 +1100

    fix/build

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,12 @@
 import { Box, Typography } from '@material-ui/core';
 import Link from 'next/link';
 
-export const ErrorPage = ({ children, title = "Suite" }) => (
-    <Box>
-        <Typography variant="h1">Error 404</Typography>
-        <Typography variant="caption">This page is not found</Typography>
-        <Link href={'/'}><a>Go back</a></Link>
-    </Box>
-);
+export default function ErrorPage({ children, title = "Suite" }) {
+    return (
+        <Box>
+            <Typography variant="h1">Error 404</Typography>
+            <Typography variant="caption">This page is not found</Typography>
+            <Link href={'/'}><a>Go back</a></Link>
+        </Box>
+    )
+};
